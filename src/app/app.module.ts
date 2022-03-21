12f14bc4d7b80bcc25eca4551ce47a0a5ccd7840
MAXIMA\eduardo.neto <eduardo.neto@maximatech.com.br>
2022-03-20 22:46:29 -0300

    pipes customizadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,15 +13,34 @@ import { CadastroComponent } from './demos/cadastro/cadastro.component';
 import {TextMask ,NgBrazil } from 'ng-brazil';
 import { CustomFormsModule } from 'ng2-validation';
 import { NavegacaoModule } from './navegacao/navegacao.module';
+import { AuthGuard } from './services/app.guard';
+import { CadastroGuard } from './services/cadastro.guard';
+import { FilmesComponent } from './demos/pipes/filmes/filmes.component';
 
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { FileSizePipe } from './demos/pipes/filmes/filesize.pipe';
+import { ImageFormaterPipe } from './demos/pipes/filmes/imageformaterpipe.pipe';
+import { Provider } from '@angular/compiler/src/core';
+import { BarDiZonesService } from './demos/bar-di-zones/bar-di-zones.service';
+import { BarDiZonesModule } from './demos/bar-di-zones/bar-di-zone.module';
+registerLocaleData(localePt);
 
+// Coleção de providers
+// pode ser utilizado para organizar varios providers e exportalos na aplicação dependente do app module
+export const BAR_ZONES_DI_PROVIDERS : Provider[] = [
+  BarDiZonesService
+];
 
 @NgModule({
   
   declarations: [
     AppComponent,
     SobreComponent,
-    CadastroComponent
+    CadastroComponent,
+    FilmesComponent,
+    FileSizePipe,
+    ImageFormaterPipe,
   ],
   imports: [
     BrowserModule,
@@ -31,9 +50,14 @@ import { NavegacaoModule } from './navegacao/navegacao.module';
     NgBrazil,
     CustomFormsModule,
     NavegacaoModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BarDiZonesModule
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    CadastroGuard,
+    //BAR_ZONES_DI_PROVIDERS
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/demos/bar-di-zones/bar-di-zone.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BarDiZonesComponent } from './bar-di-zones.component';
+
+@NgModule({
+    declarations: [],
+    imports: [ CommonModule,BarDiZonesComponent ],
+    exports: [BarDiZonesComponent],
+    providers: [],
+})
+export class BarDiZonesModule {}
